e00a00a9abcedd0d71e9998dac3e89e1d24d9baa
Mohammed Sayed <farsfhd123@gmail.com>
2023-04-14 14:46:33 +0200

    Implementing add to cart event

--- src/app/products/components/all-products/all-products.component.ts
+++ src/app/products/components/all-products/all-products.component.ts
@@ -10,6 +10,7 @@ export class AllProductsComponent implements OnInit {
   products: any[] = [];
   categories: any[] = [];
   loading: boolean = false;
+  cartProducts: any[] = [];
   constructor(private service: ProductsService) {}
 
   ngOnInit(): void {
@@ -55,5 +56,21 @@ export class AllProductsComponent implements OnInit {
       this.products = res;
       this.loading = false;
     });
+  }
+
+  addToCart(event: any) {
+    if ('cart' in localStorage) {
+      this.cartProducts = JSON.parse(localStorage.getItem('cart')!);
+      let exist = this.cartProducts.find((item) => item.id == event.id);
+      if (exist) {
+        alert(`The product is already in your cart`);
+      } else {
+        this.cartProducts.push(event);
+        localStorage.setItem('cart', JSON.stringify(this.cartProducts));
+      }
+    } else {
+      this.cartProducts.push(event);
+      localStorage.setItem('cart', JSON.stringify(this.cartProducts));
+    }
   }
 }
