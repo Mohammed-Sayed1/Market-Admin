06c3c30241e01a1fdc7c694d226bbe511015556c
Mohammed Sayed <farsfhd123@gmail.com>
2023-04-15 15:56:45 +0200

    Implementing cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { ProductsModule } from './products/products.module';
+import { CartsModule } from './carts/carts.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, ProductsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    ProductsModule,
+    CartsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/carts/services/carts.service.ts
+++ src/app/carts/services/carts.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  createNewCart(model: any) {
+    return this.http.post(`${environment.baseApi}carts`, model);
+  }
 }
